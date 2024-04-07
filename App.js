a645b7e4a910f4b29e05a849b32d41c388b682fb
bglgwyng <bglgwyng@gmail.com>
2024-04-07 17:17:39 +0900

    Initial commit.

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SharedElementExample />
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +20,39 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+const Stack = createNativeStackNavigator();
+
+function Screen1({ navigation }) {
+  return (
+    <View style={{ flex: 1 }}>
+      <Animated.View
+        style={{ width: 150, height: 150, backgroundColor: "green" }}
+        sharedTransitionTag="sharedTag"
+      />
+      <Button title="Screen2" onPress={() => navigation.navigate("Screen2")} />
+    </View>
+  );
+}
+
+function Screen2({ navigation }) {
+  return (
+    <View style={{ flex: 1, marginTop: 50 }}>
+      <Animated.View
+        style={{ width: 100, height: 100, backgroundColor: "green" }}
+        sharedTransitionTag="sharedTag"
+      />
+      <Button title="Screen1" onPress={() => navigation.navigate("Screen1")} />
+    </View>
+  );
+}
+
+function SharedElementExample() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: true }}>
+      <Stack.Screen name="Screen1" component={Screen1} />
+      <Stack.Screen name="Screen2" component={Screen2} />
+    </Stack.Navigator>
+  );
+}
